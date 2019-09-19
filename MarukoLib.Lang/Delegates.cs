c38cb613873aad9f888f5b623122fd83b6f0ccf5
Maruko <100017417@qq.com>
2019-09-19 11:38:24 +0800

    Add basic delegates and functions.

--- MarukoLib.Lang/Delegates.cs
+++ MarukoLib.Lang/Delegates.cs
@@ -1,6 +1,18 @@
 ﻿namespace MarukoLib.Lang
 {
 
+    public delegate void Consumer<in TIn>(TIn t);
+
+    public delegate TOut Supplier<out TOut>();
+
+    public delegate TOut Function<in TIn, out TOut>(TIn t);
+
+    public delegate TOut BiFunction<in TIn1, in TIn2, out TOut>(TIn1 t1, TIn2 t2);
+
+    public delegate bool BiPredicate<in TIn1, in TIn2>(TIn1 t1, TIn2 t2);
+
     public delegate T UnaryOperator<T>(T input);
-    
+
+    public delegate T BinaryOperator<T>(T t1, T t2);
+
 }
--- MarukoLib.Lang/Functions.cs
+++ MarukoLib.Lang/Functions.cs
@@ -16,6 +16,23 @@ namespace MarukoLib.Lang
 
         public static TOut Compute<TIn, TOut>(this TIn input, Func<TIn, TOut> func) => func == null ? default : func.Invoke(input);
 
+        public static sbyte Increment1(sbyte val) => (sbyte) (val + 1);
+
+        public static byte Increment1(byte val) => (byte) (val + 1);
+
+        public static short Increment1(short val) => (short) (val + 1);
+
+        public static ushort Increment1(ushort val) => (ushort) (val + 1);
+
+        public static int Increment1(int val) => val + 1;
+
+        public static uint Increment1(uint val) => val + 1;
+
+        public static long Increment1(long val) => val + 1;
+
+        public static ulong Increment1(ulong val) => val + 1;
+
+
     }
 
 }
