8c28f0e5aa74f1c9b705b8de5b194125c27502be
Lin Xinyuan <100017417@qq.com>
2019-11-12 16:42:04 +0800

    Add EndiannessUtils.InvertBits(b).

--- MarukoLib/IO/EndiannessUtils.cs
+++ MarukoLib/IO/EndiannessUtils.cs
@@ -17,11 +17,13 @@ namespace MarukoLib.IO
 
         public static readonly Endianness SystemByteOrder = BitConverter.IsLittleEndian ? Endianness.LittleEndian : Endianness.BigEndian;
 
+        public static byte InvertBits(this byte b) => (byte)~b;
+
         public static byte ReverseBits(this byte b)
         {
-            b = (byte) ((b & 0xF0) >> 4 | (b & 0x0F) << 4);
-            b = (byte) ((b & 0xCC) >> 2 | (b & 0x33) << 2);
-            b = (byte) ((b & 0xAA) >> 1 | (b & 0x55) << 1);
+            b = (byte)((b & 0xF0) >> 4 | (b & 0x0F) << 4);
+            b = (byte)((b & 0xCC) >> 2 | (b & 0x33) << 2);
+            b = (byte)((b & 0xAA) >> 1 | (b & 0x55) << 1);
             return b;
         }
 
