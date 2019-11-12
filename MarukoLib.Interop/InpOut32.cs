08e25ba309aeea52082816bea0319484ff32d392
Lin Xinyuan <100017417@qq.com>
2019-11-12 16:43:34 +0800

    Add classes 'MarukoLib.Interop.InpOut32' & 'MarukoLib.Interop.InpOutX64' and related resources.

---
+++ MarukoLib.Interop/InpOut32.cs
@@ -0,0 +1,37 @@
+﻿using System.IO;
+using System.Runtime.InteropServices;
+
+namespace MarukoLib.Interop
+{
+
+    public static class InpOut32
+    {
+
+        public const string DllName = "inpout32.dll";
+
+        private static byte[] DllBinary => Properties.Resources.inpout32;
+
+        [DllImport(DllName)]
+        public static extern void Out32(short address, short value);
+
+        [DllImport(DllName)]
+        public static extern byte Inp32(short address);
+
+        public static void ExtractDllToWorkDir() => ExtractDllToFile(DllName);
+
+        public static void ExtractDllToFile(string file)
+        {
+            if (File.Exists(file) && new FileInfo(file).Length == DllBinary.Length) return;
+            using (var stream = new FileStream(file, FileMode.OpenOrCreate))
+                WriteDll(stream);
+        }
+
+        public static void WriteDll(Stream stream)
+        {
+            var bytes = DllBinary;
+            stream.Write(bytes, 0, bytes.Length);
+        }
+
+    }
+
+}
