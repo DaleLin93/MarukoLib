dca4c5736615f075429fd9f06877babf41e2e958
Lin Xinyuan <100017417@qq.com>
2019-12-09 13:15:16 +0800

    Add LockUtils.

---
+++ MarukoLib.Threading/LockUtils.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading;
+using MarukoLib.Lang;
+
+namespace MarukoLib.Threading
+{
+    public static class LockUtils
+    {
+
+        public static IDisposable AcquireReadLock(this ReaderWriterLockSlim @lock)
+        {
+            @lock.EnterReadLock();
+            return new DelegatedDisposable(@lock.ExitReadLock);
+        }
+
+        public static IDisposable AcquireUpgradeableReadLock(this ReaderWriterLockSlim @lock)
+        {
+            @lock.EnterUpgradeableReadLock();
+            return new DelegatedDisposable(@lock.ExitUpgradeableReadLock);
+        }
+
+        public static IDisposable AcquireWriteLock(this ReaderWriterLockSlim @lock)
+        {
+            @lock.EnterWriteLock();
+            return new DelegatedDisposable(@lock.ExitWriteLock);
+        }
+
+    }
+}
