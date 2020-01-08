b41df57b97e227fe4c1b3597e27f74e20c8320a5
Lin Xinyuan <100017417@qq.com>
2020-01-08 21:19:39 +0800

    Refactor: Sequence, RandomBools, PseudoRandom.

---
+++ MarukoLib.Lang/Sequence/RandomSequence.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+namespace MarukoLib.Lang.Sequence
+{
+
+    public struct RandomTarget<T>
+    {
+
+        public RandomTarget(T value, decimal rate)
+        {
+            Value = value;
+            Rate = rate;
+        }
+
+        public static RandomTarget<T>[] Normalize(params RandomTarget<T>[] targets) => Normalize((IReadOnlyCollection<RandomTarget<T>>)targets);
+
+        public static RandomTarget<T>[] Normalize(IReadOnlyCollection<RandomTarget<T>> targets)
+        {
+            var sum = (from target in targets let rate = Math.Abs(target.Rate) where !(rate <= 0) select target.Rate).Sum();
+            return targets.Select(target => new RandomTarget<T>(target.Value, target.Rate / sum)).ToArray();
+        }
+
+        public T Value { get; }
+
+        public decimal Rate { get; }
+
+    }
+
+    public class RandomSequence<T> : ISequence<T>
+    {
+
+        private readonly Random _r;
+
+        private readonly T[] _targetValues;
+
+        private readonly decimal[] _targetBounds;
+
+        public RandomSequence(params RandomTarget<T>[] targets) : this((int)DateTimeUtils.CurrentTimeTicks, targets) { }
+
+        public RandomSequence(int seed, params RandomTarget<T>[] targets)
+        {
+            _r = new Random(seed);
+            var normalizedTargets = RandomTarget<T>.Normalize(targets);
+            TargetCount = normalizedTargets.Length;
+            _targetValues = new T[TargetCount];
+            _targetBounds = new decimal[TargetCount];
+            for (var i = 0; i < TargetCount; i++)
+            {
+                var target = normalizedTargets[i];
+                _targetValues[i] = target.Value;
+                _targetBounds[i] = (i == 0 ? 0 : _targetBounds[i - 1]) + Math.Abs(target.Rate);
+            }
+            if (TargetCount > 0) _targetBounds[TargetCount - 1] = decimal.MaxValue;
+        }
+
+        public int TargetCount { get; }
+
+        public T Next()
+        {
+            var rnd = (decimal) _r.NextDouble();
+            for (var i = 0; i < TargetCount; i++)
+                if (rnd < _targetBounds[i]) 
+                    return _targetValues[i];
+            return default;
+        }
+
+        public void Reset() { }
+
+    }
+
+    public class PseduoRandomSequence<T> : ISequence<T>
+    {
+
+        private readonly Random _r;
+
+        private readonly T[] _targetValues;
+
+        private readonly decimal[] _targetRoundCounts, _targetCounts;
+
+        public PseduoRandomSequence(params RandomTarget<T>[] targets) : this((int)DateTimeUtils.CurrentTimeTicks, targets) { }
+
+        public PseduoRandomSequence(int seed, params RandomTarget<T>[] targets)
+        {
+            _r = new Random(seed);
+            var normalizedTargets = RandomTarget<T>.Normalize(targets);
+            var minRate = normalizedTargets.Length == 0 ? 0 : normalizedTargets.Min(target => Math.Abs(target.Rate));
+            TargetCount = normalizedTargets.Length;
+            _targetValues = new T[TargetCount];
+            _targetRoundCounts = new decimal[TargetCount];
+            _targetCounts = new decimal[TargetCount];
+            for (var i = 0; i < TargetCount; i++)
+            {
+                var target = normalizedTargets[i];
+                _targetValues[i] = target.Value;
+                _targetRoundCounts[i] = Math.Abs(target.Rate) / minRate;
+            }
+        }
+
+        public int TargetCount { get; }
+
+        public T Next()
+        {
+            if (TargetCount <= 0) return default;
+            var rnd = (decimal) _r.NextDouble();
+            var choices = 0;
+            decimal roundSum = 0;
+            for (var i = 0; i < TargetCount; i++)
+            {
+                var remainingCount = _targetRoundCounts[i] - _targetCounts[i];
+                if (remainingCount <= 0) continue;
+                roundSum += remainingCount;
+                choices++;
+            }
+            Debug.Assert(choices > 0);
+            decimal accumulated = 0;
+            var lastIndex = -1;
+            for (var i = 0; i < TargetCount; i++)
+            {
+                var remainingCount = _targetRoundCounts[i] - _targetCounts[i];
+                if (remainingCount <= 0) continue;
+                lastIndex = i;
+                var rate = remainingCount / roundSum;
+                accumulated += rate;
+                if (rnd < accumulated) break;
+            }
+            _targetCounts[lastIndex] += 1;
+            if (choices == 1 && _targetCounts[lastIndex] >= _targetRoundCounts[lastIndex])
+                for (var i = 0; i < TargetCount; i++)
+                    _targetCounts[i] -= _targetRoundCounts[i];
+            return _targetValues[lastIndex];
+        }
+
+        public void Reset()
+        {
+            for (var i = 0; i < _targetCounts.Length; i++) 
+                _targetCounts[i] = 0;
+        }
+
+    }
+
+}
---
+++ MarukoLib.Lang/Sequence/Sequence.cs
@@ -0,0 +1,102 @@
+﻿using JetBrains.Annotations;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace MarukoLib.Lang.Sequence
+{
+
+    public interface ISequence<out T>
+    {
+
+        T Next();
+
+        void Reset();
+
+    }
+
+    public class DelegatedSequence<T> : ISequence<T>
+    {
+
+        [NotNull] private readonly Func<T> _nextFunc;
+
+        [CanBeNull] private readonly Action _resetFunc;
+
+        public DelegatedSequence([NotNull] Func<T> nextFunc, [CanBeNull] Action resetAction)
+        {
+            _nextFunc = nextFunc ?? throw new ArgumentNullException(nameof(nextFunc));
+            _resetFunc = resetAction;
+        }
+
+        public T Next() => _nextFunc();
+
+        public void Reset() => _resetFunc?.Invoke();
+
+    }
+
+    public sealed class EnumerableSequence<T> : ISequence<T>, IEnumerable<T>, IEnumerator<T>
+    {
+
+        private T _value;
+
+        public EnumerableSequence([NotNull] ISequence<T> source) => Source = source ?? throw new ArgumentNullException(nameof(source));
+
+        [NotNull] public ISequence<T> Source { get; }
+
+        public T Next()
+        {
+            ((IEnumerator) this).MoveNext();
+            return _value;
+        }
+
+        public void Reset() => Source.Reset();
+
+        T IEnumerator<T>.Current => _value;
+
+        object IEnumerator.Current => _value;
+
+        IEnumerator<T> IEnumerable<T>.GetEnumerator() => this;
+
+        IEnumerator IEnumerable.GetEnumerator() => this;
+
+        bool IEnumerator.MoveNext()
+        {
+            _value = Source.Next();
+            return true;
+        }
+
+        void IDisposable.Dispose() { }
+
+    }
+
+    public sealed class SyncedSequence<T> : ISequence<T>
+    {
+
+        private readonly object _lock = new object();
+
+        public SyncedSequence([NotNull] ISequence<T> source) => Source = source ?? throw new ArgumentNullException(nameof(source));
+
+        [NotNull] public ISequence<T> Source { get; }
+
+        public T Next()
+        {
+            lock (_lock) return Source.Next();
+        }
+
+        public void Reset()
+        {
+            lock (_lock) Source.Reset();
+        }
+
+    }
+
+    public static class SequenceExt
+    {
+
+        public static EnumerableSequence<T> AsEnumerable<T>(this ISequence<T> sequence) => new EnumerableSequence<T>(sequence);
+
+        public static SyncedSequence<T> AsSynced<T>(this ISequence<T> sequence) => new SyncedSequence<T>(sequence);
+
+    }
+    
+}
